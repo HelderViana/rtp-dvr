c958fcb9b9fe3dff29fca194f1f16a33e9560aab
Helder Viana <helder.d.s.viana@gmail.com>
2026-06-28 19:15:12 +0100

    Add script to github

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # rtp-dvr
 Video Recorder app for RTP Play/ RTP Palco
+
+## What is it?
+This is a "video recorder" to record live transmisions in RTP.
+If you want to use it, just clone this repo, open the .py files inside the src directory and modify the URL variable, just like in the example bellow, to use the url of RTP Play you need:
+URL
+```python
+import subprocess
+import time
+from playwright.sync_api import sync_playwright
+
+URL = "https://www.rtp.pt/play/palco/direto/rtppalco1" # --> change here to the url you need to record!
+
+```
+In this example, we have 3 scripts, used to record in parallel until 3 different live streams, for 3 different live stages, just adapt the script to point to your url adress, then it will start recording the stream and saving it under the same directory, in the format rtp_palco1_{timestamp}.ts .
+Thenif you prefer to convert it to mp4, to archive it, it would be better to convert it into mp4, you can use the following command to do it:
+
+
+URL
+```bash
+ffmpeg -i rtp_palco3_1781456969.ts -c copy rtp_Xinobi_PrimaveraSound_2026.mp4
+
+```
+### Remarks
+Feel free to use it, copy it, improve it.
---
+++ src/1rtp_playwright_record.py
@@ -0,0 +1,75 @@
+import subprocess
+import time
+from playwright.sync_api import sync_playwright
+
+
+#URL = "https://www.rtp.pt/play/direto/rtp1"
+URL = "https://www.rtp.pt/play/palco/direto/rtppalco1"
+
+
+def get_stream_and_headers():
+    stream_url = None
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,
+            args=["--no-sandbox", "--disable-dev-shm-usage"]
+        )
+
+        page = browser.new_page()
+
+        # headers base (fixos e seguros)
+        headers = {
+            "referer": URL,
+            "origin": "https://www.rtp.pt",
+            "user-agent": page.evaluate("navigator.userAgent")
+        }
+
+        def on_request(request):
+            nonlocal stream_url
+
+            if ".m3u8" in request.url and "chunklist" in request.url:
+                stream_url = request.url
+
+        page.on("request", on_request)
+
+        print("A abrir RTP Play...")
+        page.goto(URL)
+
+        # dar tempo ao player para arrancar
+        page.wait_for_timeout(15000)
+
+        browser.close()
+
+    return stream_url, headers
+
+
+def record(stream_url, headers):
+    filename = f"rtp_palco1_{int(time.time())}.ts"
+
+    cmd = [
+        "ffmpeg",
+        "-user_agent", headers.get("user-agent", "Mozilla/5.0"),
+        "-headers",
+        f"Referer: {headers.get('referer')}\r\nOrigin: {headers.get('origin')}\r\n",
+        "-i", stream_url,
+        "-c", "copy",
+        "-f", "mpegts",
+        filename
+    ]
+
+    print("A gravar:")
+    print(stream_url)
+    print(f"Ficheiro: {filename}")
+
+    subprocess.run(cmd)
+
+
+if __name__ == "__main__":
+    url, headers = get_stream_and_headers()
+
+    if not url:
+        print("Não foi possível encontrar stream (.m3u8)")
+        exit(1)
+
+    record(url, headers)
